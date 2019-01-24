d21b063c648011a4a3d98c50db1190d296bc9ce8
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2019-01-24 19:35:46 -0200

    add catch2 and ACTUAL tests, wow!

---
+++ test/util.cpp
@@ -0,0 +1,32 @@
+#if defined(WIN32)
+#define _CRT_SECURE_NO_WARNINGS
+#endif // WIN32
+
+#include "util.hpp"
+#include <cstdlib>
+
+
+std::string get_env(const char* key)
+{
+    auto* val = getenv(key);
+    return val ? val : "";
+}
+
+void set_env(const char* key, const char* value)
+{
+#ifdef WIN32
+    _putenv_s(key, value);
+#else
+    setenv(key, value, true);
+#endif // WIN32
+}
+
+void rm_env(const char* key)
+{
+#ifdef WIN32
+    _putenv_s(key, "");
+#else
+    unsetenv(key);
+#endif // WIN32
+}
+
