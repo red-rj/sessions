ef22639f68765895e58e6482f7f59970ba01d25b
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2020-05-21 18:59:57 -0300

    move sys to sys_layer.hpp

---
+++ include/sys_layer.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <type_traits>
+#include <string>
+#include <string_view>
+
+
+#ifndef WIN32
+
+extern "C" char** environ;
+
+#endif // !WIN32
+
+// system layer
+namespace red::session::sys {
+
+// env
+
+inline auto envp() noexcept
+{
+#ifdef WIN32
+    return _wenviron;
+#else
+    return environ;
+#endif
+}
+
+template<class T>
+inline size_t envsize(T** envptr) noexcept {
+    size_t size = 0;
+    while (envptr[size]) size++;
+    return size;
+}
+
+using env_t = decltype(envp());
+using envchar = std::remove_pointer_t<std::remove_pointer_t<env_t>>;
+
+std::string getenv(std::string_view key);
+void setenv(std::string_view key, std::string_view value);
+void rmenv(std::string_view key);
+
+inline constexpr char path_sep =
+#ifdef WIN32
+    ';';
+#else
+    ':';
+#endif
+
+// args
+
+char const** argv() noexcept;
+int argc() noexcept;
+
+} // namespace red::session::sys
