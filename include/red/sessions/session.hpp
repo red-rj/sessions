46ceab6007659f4698d277d0309e37314663cca5
Pedro Oliva Rodrigues <pedroolivarodrigues@outlook.com>
2020-09-07 13:43:49 -0300

    join headers

--- include/red/sessions/session.hpp
+++ include/red/sessions/session.hpp
@@ -3,16 +3,113 @@
 
 #include <type_traits>
 #include <string_view>
+#include <string>
 
 #include <range/v3/view/split.hpp>
 #include <range/v3/view/join.hpp>
 #include <range/v3/view/subrange.hpp>
 #include <range/v3/iterator/common_iterator.hpp>
+#include <range/v3/iterator/basic_iterator.hpp>
+#include <range/v3/view/transform.hpp>
 
-#include "ranges.hpp"
 #include "config.h"
 
 namespace red::session {
+
+// system layer
+namespace sys
+{
+#ifdef WIN32
+    using envchar = wchar_t;
+#else
+    using envchar = char;
+#endif
+
+    using env_t = envchar**;
+
+    env_t envp() noexcept;
+
+    std::string getenv(std::string_view key);
+    void setenv(std::string_view key, std::string_view value);
+    void rmenv(std::string_view key);
+
+    inline constexpr char path_sep =
+#ifdef WIN32
+    ';';
+#else
+    ':';
+#endif
+
+    char const** argv() noexcept;
+    int argc() noexcept;
+    
+    std::string narrow(envchar const* s);
+} // namespace sys
+
+namespace detail
+{
+    using std::ptrdiff_t;
+
+    struct environ_keyval_fn
+    {
+        template<typename Rng>
+        auto operator() (Rng&& rng, bool key) const {
+            using namespace ranges;
+            return views::transform(rng, [key](std::string const& line) {
+                auto const eq = line.find('=');
+                auto value = key ? line.substr(0, eq) : line.substr(eq+1);
+                return value;
+            });
+        }
+
+    } inline constexpr environ_keyval;
+
+    class environ_cursor
+    {
+        sys::env_t envblock = nullptr; // native environ block
+        sys::env_t mutable pos = nullptr; // last read pos.
+        std::string mutable current; // last read data
+
+    public:
+        std::string& read() const {
+            if (pos != envblock) {
+                auto* native = *envblock;
+                current = sys::narrow(native);
+                pos = envblock;
+            }
+
+            return current;
+        }
+
+        void next() {
+            envblock++;
+        }
+        void prev() {
+            envblock--;
+        }
+        void advance(ptrdiff_t n) {
+            envblock += n;
+        }
+
+        bool equal(environ_cursor const& other) const noexcept {
+            return envblock == other.envblock;
+        }
+        bool equal(ranges::default_sentinel_t ds) const noexcept {
+            return *envblock == nullptr;
+        }
+        ptrdiff_t distance_to(environ_cursor const &that) const noexcept {
+            return that.envblock - envblock;
+        }
+        
+        environ_cursor() = default;
+        environ_cursor(sys::env_t penv) : envblock(penv), pos(penv)
+        {
+            current = sys::narrow(*envblock);
+        }
+    };
+
+    using environ_iterator = ranges::basic_iterator<environ_cursor>;
+} // namespace detail
 
     class environment
     {
